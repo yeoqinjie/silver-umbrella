b5ab24641a14d63c89543fac615b957fe959d813
yeoqinjie <yeoqinjie@blinktech.sg>
2024-11-06 17:23:54 +0800

    Update code for lesson 8

--- media/js/script.js
+++ media/js/script.js
@@ -9,12 +9,20 @@ function validateForm() {
     // Select form elements
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
+    let contact = document.getElementById("contact").value;
     let errorElement = document.getElementById("error");
     let errorMessage = "";
   
     // Name field validation
     if (name === "") {
         errorMessage += "Name is required. ";
+    }
+
+    if (contact === "") {
+        errorMessage += "Contact number is required. "
+    }
+    if (contact.length !== 8) {
+        errorMessage += "Contact number must be 8 numbers. "
     }
   
     // Email format validation
@@ -25,11 +33,14 @@ function validateForm() {
   
     // Display error messages if any
     if (errorMessage) {
-        errorElement.innerHTML = errorMessage;
+        errorElement.innerHTML = errorMessage; // <p id="error">[innerHTML]</p>
         return false; // Prevent form submission
+    } else {
+        errorElement.innerHTML = 'Form Submitted'
+        return false
     }
   
-    return true; // Allow form submission
+    //return true; // Allow form submission
 }
   
 
